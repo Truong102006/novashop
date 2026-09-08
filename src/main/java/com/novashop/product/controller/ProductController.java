ddc411cdeddb4b0fbe54f5be66e9ae33c7fbfa65
truong1110 <quangtruongto2k6@gmail.com>
2026-09-08 22:52:07 +0700

    add controller

---
+++ src/main/java/com/novashop/product/controller/ProductController.java
@@ -0,0 +1,23 @@
+package com.novashop.product.controller;
+
+import com.novashop.product.entity.Product;
+import com.novashop.product.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/products")
+@RequiredArgsConstructor
+public class ProductController {
+
+    private final ProductService productService;
+
+    @GetMapping
+    public List<Product> getAllProducts() {
+        return productService.getAllProducts();
+    }
+}
---
+++ src/main/java/com/novashop/product/service/ProductService.java
@@ -0,0 +1,19 @@
+package com.novashop.product.service;
+
+import com.novashop.product.entity.Product;
+import com.novashop.product.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+}
